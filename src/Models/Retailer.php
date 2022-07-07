eb9cc058cf4426679dea49fb9d8f41476a817e32
Roy Duineveld <royduineveld@gmail.com>
2022-07-07 15:18:16 +0200

    Include the phone number by default

--- src/Models/Retailer.php
+++ src/Models/Retailer.php
@@ -49,6 +49,30 @@ class Retailer extends Model
                 );
         });
 
+        static::addGlobalScope('with-phone', function (Builder $builder) {
+            $phoneAttributeId = Cache::rememberForever('smile.phone-attribute', fn () => DB::table('eav_attribute')
+                ->select('attribute_id')
+                ->join('eav_entity_type', 'eav_entity_type.entity_type_id', '=', 'eav_attribute.entity_type_id')
+                ->where('attribute_code', 'contact_phone')
+                ->where('entity_type_code', 'smile_seller')
+                ->first()->attribute_id);
+
+            $builder
+                ->selectSub(
+                    DB::table('smile_seller_entity')
+                        ->select('value')
+                        ->join('smile_seller_entity_varchar', function ($join) use ($phoneAttributeId) {
+                            $join->on('smile_seller_entity_varchar.entity_id', '=', 'smile_seller_entity.entity_id')
+                                ->where('attribute_id', $phoneAttributeId)
+                                ->whereIn('store_id', [0, config('rapidez.store')]);
+                        })
+                        ->whereColumn('smile_seller_entity.entity_id', 'smile_retailer_address.retailer_id')
+                        ->orderByDesc('store_id')
+                        ->limit(1),
+                    'phone'
+                );
+        });
+
         static::addGlobalScope('only-active', function (Builder $builder) {
             $activeAttributeId = Cache::rememberForever('smile.active-attribute', fn () => DB::table('eav_attribute')
                 ->select('attribute_id')
